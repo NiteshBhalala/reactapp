3abc2a0c817f19514ec0b07ad4c04f975c63d758
NiteshBhalala <88238736+NiteshBhalala@users.noreply.github.com>
2022-05-13 11:51:31 +0530

    newGitCommit

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,31 @@
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+
+// const Card = (props) => {
+//     return (
+//         <>
+//             <div className="col-md-4 col-10 mx-auto">
+//                 <div class="card" >
+//                     <img src={props.imgsrc} class="card-img-top" alt="..." />
+//                     <div class="card-body">
+//                         <h5 class="card-title">{props.title}</h5>
+//                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//                         <button className="btn btn-primary">
+//                             <NavLink to="/" 
+//                             class=" button"
+//                             >Go somewhere</NavLink>
+//                         </button>
+//                     </div>
+//                 </div>
+//             </div>
+//         </>
+//     )
+// }
+
+// export default Card;
+
 import React from "react";
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 
 const Card = (props) => {
     return (
@@ -10,10 +36,15 @@ const Card = (props) => {
                     <div class="card-body">
                         <h5 class="card-title">{props.title}</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <button className="btn btn-primary">
+                        {/* <button className="btn btn-primary mainBtn">
                             <NavLink to="/" 
                             class=" button"
                             >Go somewhere</NavLink>
+                        </button> */}
+                        <button className="btn btn-outline-primary myButton">
+                            <a href="https://en.wikipedia.org/wiki/Mobile_app_development">
+                                Read More... 
+                            </a> 
                         </button>
                     </div>
                 </div>
